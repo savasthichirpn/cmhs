4cd558820a74ed6a37705cc542323ed1485652f6
Sunil Avasthi <sunil.sunilavasthi@gmail.com>
2024-07-31 19:20:13 +0530

    First Commit

---
+++ src/helper/Messages.jsx
@@ -0,0 +1,12 @@
+export const loginValidation = {
+    emailRequired: "Please enter email",
+    emailInvalid: "Email is not valid",
+    passwordRequired: "Please enter password",
+    passwordInvalid:
+        "Minimum 8 characters, must contain one number, one special character, one capital letter",
+    checkRequired: "Please check tha checkbox",
+};
+
+export const regex = {
+    emailRegex: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+};
