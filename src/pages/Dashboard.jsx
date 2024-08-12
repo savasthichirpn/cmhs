ebb027d9c0afd62ab2ed569e3a0fcb18406cbde8
Sunil Avasthi <sunil.sunilavasthi@gmail.com>
2024-08-12 23:10:57 +0530

    cards not display

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,28 +18,11 @@ import {
 import { useLoaderData } from "react-router-dom"
 
 export default function Dashboard() {
-  const tasks = useLoaderData()
-  console.log(tasks)
+  //const tasks = useLoaderData()
+  //console.log(tasks)
 
   return (
-    <SimpleGrid spacing={10} minChildWidth="300px">
-      {tasks && tasks.map((task, index) => {
-        return (
-          <Card key={index} borderRadius={'0'} borderTop="8px" borderColor="brand.400" boxShadow='md' border={'0px'} bg="white" >
-              <Flex gap={5} flexFlow={'column'} p={8}>
-              <Flex justifyContent={'center'} alignItems={'center'} borderRadius={'full'} bg={task.color} boxSize='90px'>
-                  <Img  maxW={'55px'} src={task.img} alt=''/>
-                </Flex>
-                <Box>
-                <Heading as="h3" fontSize={'24px'} fontWeight={'700'} mt={4} fontFamily={'open Sans'} color={'#000'}>{task.title}</Heading>
-                <Heading as="h4" fontSize={'18px'} fontWeight={'700'} mt={3} fontFamily={'open Sans'} color={'#000'}>{task.info}</Heading>
-                </Box>
-              </Flex>
-          </Card>
-        )
-      })
-      }
-    </SimpleGrid>
+    <div>Cards will be displayed later</div>
   )
 }
 
